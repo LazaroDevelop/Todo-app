1747d44c0326568aa2aa0e8b679b9c5c2468f963
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2024-09-30 20:02:01 -0400

    update the task list component to support dialog

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -9,11 +9,17 @@ import { DatePipe, NgFor, NgIf } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { TaskStatus } from 'src/app/models/tasks/task-status';
-import { MatButtonToggleChange, MatButtonToggleModule } from '@angular/material/button-toggle';
+import {
+  MatButtonToggleChange,
+  MatButtonToggleModule,
+} from '@angular/material/button-toggle';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatCardModule } from '@angular/material/card';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { People } from 'src/app/models/people/people';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AddPeopleComponent } from '../add-people/add-people.component';
 
 @Component({
   selector: 'app-task-list',
@@ -31,7 +37,9 @@ import { People } from 'src/app/models/people/people';
     MatButtonModule,
     MatButtonToggleModule,
     MatCardModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatDialogModule,
+    MatTooltipModule,
   ],
 })
 export class TaskListComponent implements OnInit, AfterViewInit {
@@ -45,15 +53,6 @@ export class TaskListComponent implements OnInit, AfterViewInit {
     'people',
     'delete',
   ];
-  // displayedColumns: string[] = [
-  //   'no.',
-  //   'status',
-  //   'task_name',
-  //   'final_date',
-  //   'people',
-  //   'update',
-  //   'delete',
-  // ];
   dataSource = new MatTableDataSource(this.tasks);
   selection = new SelectionModel<Task>(true, []);
   $isMobileView = false;
@@ -63,7 +62,8 @@ export class TaskListComponent implements OnInit, AfterViewInit {
   constructor(
     private storeService: StoreTaskService,
     private breakPoints: BreakpointObserver,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
@@ -106,33 +106,52 @@ export class TaskListComponent implements OnInit, AfterViewInit {
     });
   }
 
-  addPeople(){
+  triggerDialog(id: number) {
+    console.log('triggered');
+    const dialogRef = this.dialog.open(AddPeopleComponent, {
+      width: '600px',
+    });
 
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        const people: People = {
+          fullName: result.name,
+          age: result.age,
+          skills: result.skills,
+        };
+        const index = this.tasks.findIndex((t) => t.id === id);
+        this.tasks[index].people.push(people);
+        this.storeService.updateAllTasks(this.tasks);
+        this.snackBar.open('Person added to task', 'Close', {
+          duration: 2000,
+          verticalPosition: 'bottom',
+        });
+      }
+    });
   }
 
-  dropPeople(person: People){
-    if(this.tasks.length === 1){
-      this.snackBar.open('You cannot remove the last person from the task', 'Close', {
-        duration: 2000,
-        verticalPosition: 'bottom',
-      });
+  dropPeople(person: People) {
+    const index = this.tasks.findIndex((t) => t.people.includes(person));
+    if (this.tasks[index].people.length === 1) {
+      this.snackBar.open(
+        'You cannot remove the last person from the task',
+        'Close',
+        {
+          duration: 2000,
+          verticalPosition: 'bottom',
+        }
+      );
       return;
     }
-    const index = this.tasks.findIndex((t) => t.people.includes(person));
-    this.tasks[index].people = this.tasks[index].people.filter((p) => p !== person);
+
+    this.tasks[index].people = this.tasks[index].people.filter(
+      (p) => p !== person
+    );
     this.storeService.updateAllTasks(this.tasks);
     this.snackBar.open('Person removed from task', 'Close', {
       duration: 2000,
       verticalPosition: 'bottom',
     });
-
-  }
-
-  editTask(task: Task) {
-    const updateTask = this.storeService.updateTask(task);
-    const index = this.tasks.findIndex((t) => t.id === updateTask.id);
-    this.tasks[index] = updateTask;
-    this.dataSource = new MatTableDataSource(this.tasks);
   }
 
   deleteTask(task: Task) {
